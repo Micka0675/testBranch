e00a74c3dcb3e1b383b44d8bbb17b79ff23dffda
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2022-11-22 23:11:04 +0100

    Update script4.js

--- script4.js
+++ script4.js
@@ -11,10 +11,7 @@ for(multiplicateur=1; multiplicateur<11; multiplicateur++)
         for(numerateur=1; numerateur<11; numerateur++)
         {
             produit = numerateur * multiplicateur;
-            // alert(numerateur + "x " +   multiplicateur + "= "  + produit);
-            const para = document.createElement("p");
-            para.innerText = "This is a paragraph.";
-            document.body.appendChild(para);
+            alert(numerateur + "x " +   multiplicateur + "= "  + produit);
         }
 
         restart = prompt("Appuyez sur n pour interrompre l'affichage");
