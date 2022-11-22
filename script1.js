91d2015b958d8f4537cc70c725da7e3efa8b972e
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2022-11-22 15:04:32 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -116,22 +116,22 @@ function exo3(){
 
 function exo4(){
     var multiplicateur;
-    var numerateur;
-    var produit;
-    var restart = 'o';
+var numerateur;
+var produit;
+var restart = 'o';
 
+for(multiplicateur=1; multiplicateur<11; multiplicateur++)
+{
     while(restart!='n')
     {
-        for(multiplicateur=1; multiplicateur<11; multiplicateur++)
+ 
+        for(numerateur=1; numerateur<11; numerateur++)
         {
-            for(numerateur=1; numerateur<11; numerateur++)
-            {
-                produit = numerateur * multiplicateur;
-                alert(numerateur + "x " +   multiplicateur + "= "  + produit);
-            }
-
-            restart = prompt("Appuyez sur n pour interrompre l'affichage")
+            produit = numerateur * multiplicateur;
+            alert(numerateur + "x " +   multiplicateur + "= "  + produit);
         }
+
+        restart = prompt("Appuyez sur n pour interrompre l'affichage");
     }
 }
 
