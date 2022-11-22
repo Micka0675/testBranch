bf2a7cb4b30477cdc76589f5269d9c7513b85218
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2022-11-22 15:27:04 +0100

    Update script1.js

--- script1.js
+++ script1.js
@@ -110,28 +110,29 @@ function exo3(){
             alert("Programme réservé aux personnes non-retraitées");
         }
 
-        restart =prompt("Voulez-vous recommmencer  ? o/n  ")
+        restart =prompt("Voulez-vous recommmencer  ? o/n  ");
     }
 }
 
 function exo4(){
     var multiplicateur;
-var numerateur;
-var produit;
-var restart = 'o';
+    var numerateur;
+    var produit;
+    var restart = 'o';
 
-for(multiplicateur=1; multiplicateur<11; multiplicateur++)
-{
-    while(restart!='n')
+    for(multiplicateur=1; multiplicateur<11; multiplicateur++)
     {
- 
-        for(numerateur=1; numerateur<11; numerateur++)
+        while(restart!='n')
         {
-            produit = numerateur * multiplicateur;
-            alert(numerateur + "x " +   multiplicateur + "= "  + produit);
-        }
 
-        restart = prompt("Appuyez sur n pour interrompre l'affichage");
+            for(numerateur=1; numerateur<11; numerateur++)
+            {
+                produit = numerateur * multiplicateur;
+                alert(numerateur + "x " +   multiplicateur + "= "  + produit);
+            }
+
+            restart = prompt("Appuyez sur n pour interrompre l'affichage");
+        }
     }
 }
 
