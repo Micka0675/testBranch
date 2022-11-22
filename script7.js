4f6cf2022a2410afff24f0413dc7c88a21f50b15
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2022-11-22 12:06:09 +0100

    end

--- script7.js
+++ script7.js
@@ -2,4 +2,13 @@ var cote_un;
 var cote_deux;
 var hypothenuse;
 
-cote_un = prompt("Entrez un nombre entier et positif pour la longueur du premier côté")
+cote_un = prompt("Entrez un nombre entier et positif pour la longueur du premier côté");
+cote_un = cote_un * cote_un;
+
+cote_deux = prompt("Entrez un nombre entier et positif pour la longueur du premier côté");
+cote_deux = cote_deux * cote_deux;
+
+hypothenuse = cote_un + cote_deux;
+hypothenuse = Math.sqrt(hypothenuse);
+
+alert("La longueur de l'hypothénuse est " + hypothenuse + ".");
