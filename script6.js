7d4585b557ec80d379eee50255a6f7728c726f5f
Mickael R <94828882+Micka0675@users.noreply.github.com>
2022-11-22 14:51:03 +0100

    Delete script6.js

--- script6.js
+++
@@ -1,7 +0,0 @@
-var impair = 1;
-document.write("<p>" + impair + " est le premier nombre impair</p>");
-for(i = 1 ; i <= 19; i++)
-{
-    impair = impair + 2;
-    document.write("<p>"+ impair + " est le "+ parseFloat(i+1) + " ème nombre impair</p>");
-}
